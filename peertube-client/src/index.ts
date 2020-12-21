54f3ee5522fca2838153ca3a47bcac62a0af823e
AkiraFukushima <h3.poteto@gmail.com>
2020-12-21 22:35:34 +0900

    Add peertube-client example

---
+++ peertube-client/src/index.ts
@@ -0,0 +1,32 @@
+import { Peertube } from "peertube-client";
+
+//------------------------------
+// To resolve fetch in node js
+//------------------------------
+import Global = NodeJS.Global;
+export interface GlobalWithCognitoFix extends Global {
+  fetch: any;
+}
+declare const global: GlobalWithCognitoFix;
+global.fetch = require("node-fetch").default;
+//------------------------------
+
+declare var process: {
+  env: {
+    PEERTUBE_USER: string;
+    PEERTUBE_PASSWORD: string;
+  };
+};
+
+const peertube = new Peertube({
+  instance: "peertube.fr",
+  user: process.env.PEERTUBE_USER,
+  password: process.env.PEERTUBE_PASSWORD,
+});
+
+const main = async () => {
+  const videos = await peertube.getVideos();
+  console.log(videos);
+};
+
+main();
